cdf7975ec19b55a4bdade3f51f62813d9254c835
Adam Šima <adamsima77@gmail.com>
2026-03-22 09:20:45 +0100

    Nuxt.js configuration

---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class = "text-center text-black text-2xl">Hello World !</div>
+</template>
+
+<script setup>
+
+</script>
--- app/stores/counter.js
+++ app/stores/counter.js
@@ -1,12 +1,20 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
 
 export const useCounterStore = defineStore('counter', () => {
+  //State 
   const count = ref(0)
+  //Getter
   const doubleCount = computed(() => count.value * 2)
+
+  //Actions
   function increment() {
     count.value++
   }
 
-  return { count, doubleCount, increment }
+  function decrement() {
+    count.value--
+  }
+
+  return {count,doubleCount,increment,decrement}
 })
